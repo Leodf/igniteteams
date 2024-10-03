d22f44105d4e5efb5979e9b349f0579831134800
Leonardo de Faveri <faver_i@hotmail.com>
2024-10-03 18:55:04 -0300

    add phospor icons and create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 import LogoIMg from "@assets/logo.png";
 
-export default function Header() {
+type HeaderProps = {
+  showBackButton?: boolean;
+};
+
+export default function Header({ showBackButton = false }: HeaderProps) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
       <Logo source={LogoIMg} />
     </Container>
   );
