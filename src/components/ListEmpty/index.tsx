dc4f74e20ccc2076940c7f86a71a82c34ecbc43a
Leonardo de Faveri <faver_i@hotmail.com>
2024-10-09 18:01:53 -0300

    create ListEmpty component and add Flatlis on Groups screen

---
+++ src/components/ListEmpty/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Message } from "./styles";
+
+type ListEmptyProps = {
+  message: string;
+};
+
+export default function ListEmpty({ message }: ListEmptyProps) {
+  return (
+    <Container>
+      <Message>{message}</Message>
+    </Container>
+  );
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "./styles";
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
 import GroupCard from "@components/GroupCard";
+import { FlatList } from "react-native";
+import ListEmpty from "@components/ListEmpty";
 
 export default function Groups() {
+  const [groups, setGroups] = useState([]);
   return (
     <>
       <Container>
         <Header />
         <Highlight title="Turmas" subtitle="jogue com sua turma" />
-        <GroupCard title="Nova turma" />
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <GroupCard title={item} />}
+          ListEmptyComponent={() => (
+            <ListEmpty message="Que tal cadastrar a primeira turma" />
+          )}
+          contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        />
       </Container>
     </>
   );
